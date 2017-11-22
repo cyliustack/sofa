ede9ffd4e82d5b54dc52e1027c9cbf4a7491b085
cyliustack <cyliustack@gmail.com>
2017-11-23 01:12:08 +0800

    Update install

--- install.sh
+++ install.sh
@@ -1,14 +1,45 @@
 #!/bin/bash	
-rm -rf /opt/sofa
+for i in "$@"
+do
+case $i in
+    -e=*|--prefix=*)
+    PREFIX="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+if [[ ${PREFIX} == "" ]]; then
+    echo "Usage: ./install.sh --prefix=/directory/to/install"
+    read -p "Use default path /opt/sofa ?(Y/n) " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        PREFIX=/opt/sofa 
+        echo ""   
+    else
+        echo ""
+        echo "Goodbye." 
+        exit -1 
+    fi 
+    
+fi
+
+echo " Installation directory is ${PREFIX}"
+
+set -x
+rm -rf ${PREFIX}
 rm -f /usr/local/bin/sofastat.py
 rm -f /usr/local/bin/sofa
-mkdir -p /opt/sofa/bin
-mkdir -p /opt/sofa/sofaboard
-mkdir -p /opt/sofa/plugin
-cp -i sofa /opt/sofa/bin
-cp -i sofastat.py /opt/sofa/bin
-cp -f sofaboard/index.html /opt/sofa/sofaboard
-cp -f sofaboard/gpu-report.html /opt/sofa/sofaboard
-ln -is /opt/sofa/bin/sofa /usr/local/bin/sofa
-ln -is /opt/sofa/bin/sofastat.py /usr/local/bin/sofastat.py
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/sofaboard
+mkdir -p ${PREFIX}/plugin
+cp -i sofa                      ${PREFIX}/bin
+cp -i sofastat.py               ${PREFIX}/bin
+cp -f sofaboard/index.html      ${PREFIX}/sofaboard
+cp -f sofaboard/gpu-report.html ${PREFIX}/sofaboard
+ln -is ${PREFIX}/bin/sofa           /usr/local/bin/sofa
+ln -is ${PREFIX}/bin/sofastat.py    /usr/local/bin/sofastat.py
 
