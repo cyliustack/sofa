bf69ae29bfdc76d60391ac6e599fa2729ddc3494
Your Name <you@example.com>
2017-11-30 20:03:42 +0800

    add beta version of sofa-analyze

--- sofa-analyze.py
+++ sofa-analyze.py
@@ -4,13 +4,14 @@ import pandas as pd
 import numpy as np
 import csv
 import random
+from operator import itemgetter, attrgetter
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv) 
 logdir = []
 filein = []
 
 if len(sys.argv) < 2:
-    print("Usage: sofa-report.py /path/to/logdir")
+    print("Ustimestamp: sofa-report.py /path/to/logdir")
     quit();
 else:
     logdir = sys.argv[1] + "/"
@@ -23,7 +24,6 @@ print(df.groupby("deviceId")["data_B"].sum()/1000000)
 print("Execution Time for each Device (MB)");
 print(df.groupby("deviceId")["duration"].sum())
 
-
 print("Data Traffic for each CopyKind (MB)");
 data_copyKind =  df.groupby("copyKind")["data_B"].sum()/1000000
 print(data_copyKind)
@@ -32,11 +32,49 @@ print("Data Traffic Overhead for each CopyKind");
 durations_copyKind = df.groupby("copyKind")["duration"].sum()
 print(durations_copyKind)
 
-print("Data Traffic Overhead for each CopyKind");
+print("Overhead brought from Each Stream");
 stream_durations = df.groupby("streamId")["duration"].sum()
 print(stream_durations)
 
-
-print("Averaged Achieved Bandwidth for each CopyKind: (GB/s)") 
+print("Avertimestampd Achieved Bandwidth for each CopyKind: (GB/s)") 
 bw = data_copyKind/durations_copyKind/1000
-print(bw)	
+print(bw)
+
+print("Overlapness for All Events (s)");
+class Event:
+        def __init__(self, name, ttype, timestamp):
+                self.name = name
+                self.ttype = ttype # 0 for begin, 1 for end
+                self.timestamp = timestamp
+        def __repr__(self):
+                return repr((self.name, self.ttype, self.timestamp))
+
+
+events = []
+for i in range(len(df)):
+	#print("df[%d]=%lf" % (i,df.iloc[i]['time']))
+	e = Event(i,0,i)
+	events.append(e)
+	e = Event(i,1,i+0.5*random.randint(1,10))
+	events.append(e)
+events.sort(key=attrgetter('timestamp'))
+#sorted(events, key=lambda events: events.ttype)
+#events.sort()
+event_stack=[]
+overlaptime=0
+for e in events:
+	if e.ttype == 0:
+		event_stack.append(e)
+	if e.ttype == 1:
+		print("reach end of time for event-%d"%(e.name))
+		# find all the previous event with 
+		for es in event_stack:
+			print("top of event_stack: name:%d"%(event_stack[len(event_stack)-1].name))
+			overlaptime = overlaptime + (e.timestamp - es.timestamp)
+				
+	print("e.t = %lf, e.ttype = %d" % (e.timestamp,e.ttype))
+	
+print("overlapped time: %lf"%(overlaptime))
+
+
+
