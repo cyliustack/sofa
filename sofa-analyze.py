5c1d71db4b26f1827a7bff11ca69e621f8345847
Your Name <you@example.com>
2017-12-04 00:40:37 +0800

    support all-to-all overlapped time

--- sofa-analyze.py
+++ sofa-analyze.py
@@ -6,75 +6,90 @@ import csv
 import random
 from operator import itemgetter, attrgetter
 print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv) 
+print 'Argument List:', str(sys.argv)
 logdir = []
 filein = []
 
 if len(sys.argv) < 2:
     print("Ustimestamp: sofa-report.py /path/to/logdir")
-    quit();
+    quit()
 else:
     logdir = sys.argv[1] + "/"
-    filein = logdir+"gputrace.csv"
+    filein = logdir + "gputrace.csv"
 
 df = pd.read_csv(filein)
-print("Data Traffic for each Device (MB)");
-print(df.groupby("deviceId")["data_B"].sum()/1000000)
+print("Data Traffic for each Device (MB)")
+print(df.groupby("deviceId")["data_B"].sum() / 1000000)
 
-print("Execution Time for each Device (MB)");
+print("Execution Time for each Device (MB)")
 print(df.groupby("deviceId")["duration"].sum())
 
-print("Data Traffic for each CopyKind (MB)");
-data_copyKind =  df.groupby("copyKind")["data_B"].sum()/1000000
+print("Data Traffic for each CopyKind (MB)")
+data_copyKind = df.groupby("copyKind")["data_B"].sum() / 1000000
 print(data_copyKind)
 
-print("Data Traffic Overhead for each CopyKind");
+print("Data Traffic Overhead for each CopyKind")
 durations_copyKind = df.groupby("copyKind")["duration"].sum()
 print(durations_copyKind)
 
-print("Overhead brought from Each Stream");
+print("Overhead brought from Each Stream")
 stream_durations = df.groupby("streamId")["duration"].sum()
 print(stream_durations)
 
-print("Avertimestampd Achieved Bandwidth for each CopyKind: (GB/s)") 
-bw = data_copyKind/durations_copyKind/1000
+print("Avertimestampd Achieved Bandwidth for each CopyKind: (GB/s)")
+bw = data_copyKind / durations_copyKind / 1000
 print(bw)
 
-print("Overlapness for All Events (s)");
-class Event:
-        def __init__(self, name, ttype, timestamp):
-                self.name = name
-                self.ttype = ttype # 0 for begin, 1 for end
-                self.timestamp = timestamp
-        def __repr__(self):
-                return repr((self.name, self.ttype, self.timestamp))
+print("Overlapness for All Events (s)")
 
+# Assume pa<pb, pc<pd:
+def overlap(pa, pb, pc, pd):
+    if pb - pc >= 0 and pd - pa >=0:   
+        return  min(pb, pd) - max(pa, pc)
+
+class Event:
+    def __init__(self, name, ttype, timestamp, duration):
+        self.name = name
+        self.ttype = ttype  # 0 for begin, 1 for end
+        self.timestamp = timestamp
+        self.duration = duration
+
+    def __repr__(self):
+        return repr((self.name, self.ttype, self.timestamp, self.duration))
 
 events = []
 for i in range(len(df)):
-	#print("df[%d]=%lf" % (i,df.iloc[i]['time']))
-	e = Event(i,0,i)
-	events.append(e)
-	e = Event(i,1,i+0.5*random.randint(1,10))
-	events.append(e)
+    t_begin = df.iloc[i]['time'] 
+    d = df.iloc[i]['duration'] 
+    t_end = t_begin + d 
+    e = Event(i, 0, t_begin, d)
+    events.append(e)
+    e = Event(i, 1, t_end, d)
+    events.append(e)
+#for i in range(3):
+#    #print("df[%d]=%lf" % (i,df.iloc[i]['time']))
+#    t_begin =   i 
+#    d = 0.5 * random.randint(1, 10)
+#    t_end = t_begin + d 
+#    e = Event(i, 0, t_begin, d)
+#    events.append(e)
+#    e = Event(i, 1, t_end, d)
+#    events.append(e)
 events.sort(key=attrgetter('timestamp'))
-#sorted(events, key=lambda events: events.ttype)
-#events.sort()
-event_stack=[]
-overlaptime=0
+
+event_stack = []
+overlaptime = 0
 for e in events:
-	if e.ttype == 0:
-		event_stack.append(e)
-	if e.ttype == 1:
-		print("reach end of time for event-%d"%(e.name))
-		# find all the previous event with 
-		for es in event_stack:
-			print("top of event_stack: name:%d"%(event_stack[len(event_stack)-1].name))
-			overlaptime = overlaptime + (e.timestamp - es.timestamp)
-				
-	print("e.t = %lf, e.ttype = %d" % (e.timestamp,e.ttype))
-	
-print("overlapped time: %lf"%(overlaptime))
-
-
-
+    #print(event_stack)
+    if e.ttype == 0:
+        event_stack.append(e)
+    if e.ttype == 1:
+        #print("reach end of time for event-%d" % (e.name))
+        # find all the previous event with
+        for es in event_stack:
+            if es.name != e.name:
+                #print("n:%d t:%lf d:%lf overlaptime:%lf" % (es.name, es.timestamp, es.duration, overlaptime))
+                overlaptime = overlaptime + overlap(es.timestamp,es.timestamp+es.duration, e.timestamp-e.duration,e.timestamp)
+        #print("pop out %d" % e.name)
+        event_stack = [es for es in event_stack if es.name != e.name]
+print("overlapped time of Events: %lf" % (overlaptime))
