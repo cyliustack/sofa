f9e8509da8863da90c300a83ccf1cbc5000bf273
cyliustack <cyliustack@gmail.com>
2017-12-14 20:20:10 +0800

    Fix bug when no gpus are used.

--- sofa-preprocess.py
+++ sofa-preprocess.py
@@ -228,8 +228,10 @@ try:
 except sqlite3.OperationalError:
     #print_warning("Cannot find CUPTI_ACTIVITY_KIND_CONCURRENT_KERNEL")
     try:
-        cursor.execute(
-            "SELECT start,end,name,streamId,deviceId FROM CUPTI_ACTIVITY_KIND_KERNEL")
+        cursor.execute("SELECT start,end,name,streamId,deviceId FROM CUPTI_ACTIVITY_KIND_KERNEL")
+    except sqlite3.OperationalError:
+        #print_warning("Cannot find CUPTI_ACTIVITY_KIND_KERNEL")
+        quit()
 
 records = cursor.fetchall()
 i = 0
