366b9d468f36ec120796189830b5ab2d5509d672
Medicine Yeh <medicinehy@gmail.com>
2018-01-30 22:39:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,45 @@
-#!/bin/bash	
+#!/bin/bash
+
+SCRIPT_PATH="$(readlink -f "$(dirname "$0")")"
+
+C_NONE="\033[0;00m"
+C_RED="\033[1;31m"
+C_GREEN="\033[1;32m"
 
 print_help(){
-    echo "Usage: ./install.sh /path/to/directory/of/sofa"
+    echo "This script will install the sofa scripts to your system under the specified <PREFIX> directory."
+    echo ""
+    echo "Usage: $0 PREFIX"
+    echo "Examples: $0 ~/bin/sofa"
+    echo "          sudo $0 /opt/bin/sofa"
 }
 
+function clear_install_dir()
+{
+    echo "Creating directories..."
+    mkdir -p ${PREFIX}
+    mkdir -p ${PREFIX}/bin
+    mkdir -p ${PREFIX}/sofaboard
+    mkdir -p ${PREFIX}/plugins
+    mkdir -p ${PREFIX}/tools
+    rm -f ${PREFIX}/bin/*.pyc
+}
+
+function install_sofa()
+{
+    echo "Installing..."
+    cp -rf ${SCRIPT_PATH}/bin       ${PREFIX}
+    cp -rf ${SCRIPT_PATH}/plugins   ${PREFIX}
+    cp -rf ${SCRIPT_PATH}/sofaboard ${PREFIX}
+
+    # Create a new file for configurations
+################## heredoc style
+    cat > ${PREFIX}/tools/activate.sh <<EOF
+export PATH=${PREFIX}/bin:\$PATH
+export PATH=\$PATH:/usr/local/cuda/bin
+EOF
+##################
+}
 
 for i in "$@"
 do
@@ -12,50 +48,32 @@ case $i in
         print_help
         exit 0
     ;;
-    *)  
+    *)
         # unknown option
     ;;
 esac
 done
 
-PREFIX=$1
-
-if [[ ${PREFIX} == "" ]]; then
+if [[ "$1" == "" ]]; then
+    echo -e "${C_RED}Please specify the install directory!${C_NONE}"
     print_help
-    read -p "Use default path /opt/sofa ?(Y/n) " -n 1 -r
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then        
-        PREFIX=/opt/sofa 
-        echo ""   
-    else
-        echo ""
-        echo "Try './install.sh {-h|--help}' for more information."
-        exit -1 
-    fi 
-    
+    exit 1
 fi
 
-echo " Installation directory is ${PREFIX}"
-set -x
-mkdir -p ${PREFIX}
-mkdir -p ${PREFIX}/bin
-mkdir -p ${PREFIX}/sofaboard
-mkdir -p ${PREFIX}/plugin
-mkdir -p ${PREFIX}/tools
-rm ${PREFIX}/bin/*.pyc
-cp -f sofa                      ${PREFIX}/bin
-cp -f sofa-preprocess.py        ${PREFIX}/bin
-cp -f sofa-analyze.py           ${PREFIX}/bin
-cp -f sofa_print.py             ${PREFIX}/bin
-cp -f sofa_config.py            ${PREFIX}/bin
-cp -f sofaboard/index.html          ${PREFIX}/sofaboard
-cp -f sofaboard/timeline.js         ${PREFIX}/sofaboard
-cp -f sofaboard/cpu-report.html     ${PREFIX}/sofaboard
-cp -f sofaboard/gpu-report.html     ${PREFIX}/sofaboard
-cp -f sofaboard/overhead.html          ${PREFIX}/sofaboard
-cp -f sofaboard/comm-report.html          ${PREFIX}/sofaboard
-echo "export PATH=\$PATH:${PREFIX}/bin" > tools/activate.sh
-echo "export PATH=\$PATH:/usr/local/cuda/bin" >> tools/activate.sh
-cp -f tools/activate.sh   ${PREFIX}/tools
-set +x
-echo "Please try 'source ${PREFIX}/tools/activate.sh' to enjoy sofa!"
+# Get the first argument and also remove the last / if present
+PREFIX="${1%/}"
+# Add sofa to PREFIX if not present
+[[ $(basename "$PREFIX") != "sofa" ]] && PREFIX="${PREFIX}/sofa"
+PREFIX=$(readlink -f "$PREFIX")
+
+echo -e "${C_GREEN}Installation directory is ${PREFIX}${C_NONE}"
+
+# Print every executed command for debugging
+set -x -e
+clear_install_dir
+install_sofa
+# Disable printing every executed command for debugging
+set +x +e
+
+echo -e "${C_GREEN}Please try 'source ${PREFIX}/tools/activate.sh' to enjoy sofa!${C_NONE}\n"
+echo -e "${C_GREEN}Add 'source ${PREFIX}/tools/activate.sh' to your ~/.bashrc if you want to enable sofa on every shells.${C_NONE}\n\n"
