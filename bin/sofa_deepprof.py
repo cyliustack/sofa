6f9714129bb8f16e3d4fa16d56d8e51df6cdf477
cyliustack <cyliustack@gmail.com>
2018-06-19 14:56:46 +0800

    Support auto top-K feature construction for each function swarm

--- bin/sofa_deepprof.py
+++ bin/sofa_deepprof.py
@@ -15,10 +15,9 @@ from functools import partial
 from sofa_print import *
 from sofa_config import *
 from STree import *
+import re
 
-#events = ["HtoD", "DtoH", "DtoD", "PtoP"]
-#DEMO: Execute "samples/1_Utilities/bandwidthTest/bandwidthTest" three times 
-eventName = ['gpu1_copyKind_1_33554432B','gpu1_copyKind_2_33554432B','gpu1_copyKind_8_33554432B']
+
 iteration_begin = 0
 iteration_end = 0
 iteration_timelines = []
@@ -27,18 +26,31 @@ iteration_table = []
 
 base_time = 0
 
+def get_top_k_events(df, topk):
+    topk_events=[]
+    print("Top %d Events:"%topk)
+    gby = df.groupby(['name'])
+    df_agg = gby.aggregate(np.sum)
+    df_agg_sorted = df_agg.sort_values(by=['duration'],ascending=False)
+    #eventName = ['gpu1_copyKind_1_','gpu1_copyKind_2_','gpu1_copyKind_8_']
+    eventName = df_agg_sorted[df_agg_sorted.columns[0:0]].head(topk).index.values.tolist()
+    for i in range(len(eventName)):
+        print('[%d] %s'%(i,eventName[i]))
+    return eventName
+
 def iterationDetection(logdir, cfg, df_gpu, time_interval, threshold, iteration_times):
     global iteration_begin, iteration_end, iteration_index, iteration_timelines, iteration_table, base_time
     time_begin = 0
     time_end = int(round(df_gpu.iloc[-1]['timestamp']/time_interval))
-    events = eventName[:]
-    events.append("timestamp")
+    event_names = get_top_k_events(df_gpu,10)
+    events = event_names[:]
+    events.append('timestamp')
     patternTable = pd.DataFrame(columns = events, dtype = int)	
     candidate_pattern=[]
     iteration_pattern_count = 1 
     time = 0
     n = 0
-    k = 2
+    k = 1 
     #Create pattern table by extract top-k feature, and label each vector with specified index 
     while(time < time_end):
         clock = time + 1
@@ -47,18 +59,18 @@ def iterationDetection(logdir, cfg, df_gpu, time_interval, threshold, iteration_
         
         #Create vector and count 
         vector = []
-        for e in eventName:
+        for e in event_names:
             count = eventCount('name', e, df_block)
             vector.append(count)
         if sum(vector)==0:
             #if vector is empty
-            iteration_timelines.append('0')
+            iteration_timelines.append('0,')
         else:
             patternMatch = patternMatching(patternTable, vector, threshold)
             if patternMatch != -1:
-                iteration_timelines.append(str(iteration_index))
+                iteration_timelines.append(str(iteration_index)+',')
             else:
-                iteration_timelines.append(str(iteration_pattern_count))
+                iteration_timelines.append(str(iteration_pattern_count)+',')
                 iteration_pattern_count += 1
                 vector.append(time)
                 vectorSerie = pd.Series(vector, index = events)
@@ -67,19 +79,26 @@ def iterationDetection(logdir, cfg, df_gpu, time_interval, threshold, iteration_
         time += 1
 
     #building suffix tree to find patter0
-    print(iteration_timelines)
+    #print(iteration_timelines)
     mainString = "".join(iteration_timelines)
+    
+    ### Worker Intelligence
+    #n_matches = len(re.findall('9,10,0,0,', mainString)) + \
+    #            len(re.findall('10,14,0,0,', mainString)) + \
+    #            len(re.findall('9,15,0,0,', mainString))  
+    #print("matched times = %d"%n_matches)
+    
+    #mainString='11122231111222311112202'
+    #mainString = 'aabbcccaabbcccaabbccc' 
     st = STree(mainString)
     print(mainString)
     IT_times = iteration_times
-    while not candidate_pattern:
-        if(IT_times == 0):
-            print("can't find pattern")
-            return 0
-        st.find_repeat_pattern(candidate_pattern, IT_times)
-        IT_times -= 1
-    #pattern finded    
+    st.find_repeat_pattern(candidate_pattern, IT_times)
+    print("pattern",candidate_pattern)
+    #pattern finded
+    print(candidate_pattern)
     pattern = max(candidate_pattern, key = len)
+    print("pattern is "+pattern)
     #reverse main string and pattern then do approximate match by cutting main string to n+k block, where n is iteration_times, k is akon's heart mothod(TODO: k can set by evaluate hardware config). the reason why we doing this is because in DL models, the warm-up stage may cost more time then stable state iteration. 
     reversePattern = pattern[::-1]
     reverseMainString = mainString[::-1]
@@ -96,7 +115,7 @@ def iterationDetection(logdir, cfg, df_gpu, time_interval, threshold, iteration_
         blockString = reverseMainString[block_beg:block_end]
         #use fuzzywuzzy as approximate match accuracy. TODO: use reasonable threshold.
         if fuzz.token_sort_ratio(blockString, reversePattern) > 70:
-            iteration_table.append((float(total_length - 1 - block_end) / time_interval, float(total_length - 1 -block_beg) / time_interval))     
+            iteration_table.append((float(total_length - 1 - block_end) / time_interval, float(total_length - 1 -block_beg) / time_interval))
             block_beg += block_size
             block_end = block_beg + block_size
             shrink_size = 0
@@ -124,24 +143,24 @@ def patternMatching(patternTable, vector, threshold):
     
     global iteration_index
     if patternTable.empty:
-        return -1 
+        return -1
     for index, row in patternTable.iterrows():
         #pvector is vector from patternTable without timestamp
         pvector = row.values[:-1]
-        sim = similarity(pvector, vector)
-        if sim >= threshold:
-            iteration_index = index+1
+        if similar(pvector, vector, threshold):
+            iteration_index = index + 1
             return 0
     return -1
 
-def similarity(a, b):
-    maxv = max(np.linalg.norm(a), np.linalg.norm(b))
-    minv = min(np.linalg.norm(a), np.linalg.norm(b))
-    if (maxv/minv)<0.5:
-        print(maxv/minv)
-        return 0
+def similar(a, b, threshold): 
     result = 1. - spatial.distance.cosine(a, b)
-    return result
+    if result > threshold: 
+        maxv = max(np.linalg.norm(a), np.linalg.norm(b))
+        minv = min(np.linalg.norm(a), np.linalg.norm(b))
+        ratio = np.abs(maxv/minv) 
+        if ratio < 1.2 and ratio > 0.8:
+            return True
+    return False
 
  
 def traces_to_json(path):
@@ -178,7 +197,7 @@ def sofa_deepprof(logdir, cfg, df_cpu, df_gpu):
     try:
         base_time = df_gpu.loc[0,'timestamp']
         df_gpu.loc[:,'timestamp'] -= df_gpu.loc[0,'timestamp']
-        iterationDetection(logdir, cfg, df_gpu, 0.1, 0.7, 3)
+        iterationDetection(logdir, cfg, df_gpu, 0.01, 0.7, 20)
         iteration_begin += base_time
         iteration_end += base_time 
         traces_to_json(logdir + 'report.js')
