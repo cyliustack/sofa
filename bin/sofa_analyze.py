2a307afce1e8181cd19eea1d56137b17e63b6a80
Chih-Hao Wei <chih-hao.wei@adlinktech.com>
2018-06-19 13:28:20 +0800

    fix bugs and detect with brute-force

--- bin/sofa_analyze.py
+++ bin/sofa_analyze.py
@@ -362,17 +362,17 @@ def sofa_analyze(logdir, cfg):
                         #print('%d connects to %d' % (i, j-1))
             #print(edges)
             G = nx.DiGraph(edges)           
-            for cycle in nx.simple_cycles(G):
-                if len(cycle) == num_gpus:
-                    print(("One of the recommended %d rings" % len(cycle) ))
-                    print(cycle)
-                    os.system("mkdir -p /tmp/sofa_hints/")
-                    xring_order = ','.join(map(str, cycle))
-                    with open("/tmp/sofa_hints/xring_order.txt", "w") as f:
-                        f.write('export CUDA_VISIBLE_DEVICES=' + xring_order)
+            #for cycle in nx.simple_cycles(G):
+            #    if len(cycle) == num_gpus:
+            #        print(("One of the recommended %d rings" % len(cycle) ))
+            #        print(cycle)
+            #        os.system("mkdir -p /tmp/sofa_hints/")
+            #        xring_order = ','.join(map(str, cycle))
+            #        with open("/tmp/sofa_hints/xring_order.txt", "w") as f:
+            #            f.write('export CUDA_VISIBLE_DEVICES=' + xring_order)
                     # pathlib.Path('/tmp/sofa_hints/xring_order.txt').write_text('export CUDA_VISIBLE_DEVICES=' + xring_order)  # UPGRADE: py35
                     # pathlib.Path('/tmp/sofa_hints/xring_order.txt').write_text(f'export CUDA_VISIBLE_DEVICES=f{xring_order}')  # UPGRADE: py36
-                    break
+            #        break
     #try:
     #    df_mpstat = pd.read_csv(filein_mpstat)
     #    mpstat_profile(logdir, cfg, df_mpstat)
@@ -392,9 +392,9 @@ def sofa_analyze(logdir, cfg):
 
     try:
         df_gpu = pd.read_csv(filein_gpu)
+        sofa_deepprof(logdir, cfg, df_cpu, df_gpu)  
         df_gpu.loc[:, 'timestamp'] -= df_gpu.loc[0, 'timestamp']
         gpu_profile(logdir, cfg, df_gpu)
-        sofa_deepprof(logdir, cfg, df_cpu, df_gpu)  
     except IOError:
         print_warning(
             "gputrace.csv is not found. If there is no need to profile GPU, just ignore it.")
--- bin/sofa_deepprof.py
+++ bin/sofa_deepprof.py
@@ -23,11 +23,12 @@ iteration_end = 0
 iteration_timelines = []
 iteration_index = 1
 iteration_table = []
+blank_count = 0
 
 base_time = 0
 
 def iterationDetection(logdir, cfg, df_gpu, time_interval, threshold, iteration_times):
-    global iteration_begin, iteration_end, iteration_index, iteration_timelines, iteration_table, base_time
+    global iteration_begin, iteration_end, iteration_index, iteration_timelines, iteration_table, base_time, blank_count
     time_begin = 0
     time_end = int(round(df_gpu.iloc[-1]['timestamp']/time_interval))
     events = eventName[:]
@@ -51,15 +52,18 @@ def iterationDetection(logdir, cfg, df_gpu, time_interval, threshold, iteration_
         
         if sum(vector)==0:
             #if vector is empty
-            iteration_timelines.append('0')
+            if not patternTable.empty: 
+                iteration_timelines.append('0,')
+            else:
+                blank_count += 1
             time += 1
             continue
         patternMatch = patternMatching(patternTable, vector, threshold)
         if patternMatch != -1:
-            iteration_timelines.append(str(iteration_index))
+            iteration_timelines.append(str(iteration_index)+",")
             
         else:
-            iteration_timelines.append(str(iteration_pattern_count))
+            iteration_timelines.append(str(iteration_pattern_count)+",")
             iteration_pattern_count += 1
             vector.append(time)
             vectorSerie = pd.Series(vector, index = events)
@@ -71,49 +75,41 @@ def iterationDetection(logdir, cfg, df_gpu, time_interval, threshold, iteration_
     mainString = "".join(iteration_timelines)
     st = STree(mainString)    
     IT_times = iteration_times
-    while not candidate_pattern:
-        if(IT_times == 0):
-            print("can't find pattern")
-            return 0
-        st.find_repeat_pattern(candidate_pattern, IT_times)
-        IT_times -= 1
-    #pattern finded    
+#    while not candidate_pattern:
+#        if(IT_times == 0):
+#            print("can't find pattern")
+#            return 0
+    st.find_repeat_pattern(candidate_pattern, IT_times)
+#        IT_times -= 1
+    #pattern finded  
+    print("iteration_timelines:", iteration_timelines)
+    print("mainString:", mainString) 
+    print("candidate_pattern:",candidate_pattern)
     pattern = max(candidate_pattern, key = len)
+    print("pattern:", pattern)
     #reverse main string and pattern then do approximate match by cutting main string to n+k block, where n is iteration_times, k is akon's heart mothod(TODO: k can set by evaluate hardware config). the reason why we doing this is because in DL models, the warm-up stage may cost more time then stable state iteration. 
-    reversePattern = pattern[::-1]
-    reverseMainString = mainString[::-1]
     total_length = len(mainString)
-    block_size = total_length / (n + k)
+    block_size = len(pattern)
     process = 0
     block_beg = 0
     block_end = block_size
-    shrink_size = 0
-    #TODO: set reasonable rate.
-    shrink_rate = int(round(1 / time_interval / 10)) 
     iteration_count = 0
-    while process < total_length:
-        blockString = reverseMainString[block_beg:block_end]
+    fuzzyRatioTable = []
+    print("black_count:",blank_count)
+    while process < (total_length - block_size):
+        blockString = mainString[block_beg:block_end]
         
         #use fuzzywuzzy as approximate match accuracy. TODO: use reasonable threshold.
-        if fuzz.token_sort_ratio(blockString, reversePattern) > 70:
-            iteration_table.append((float(total_length - 1 - block_end) / time_interval, float(total_length - 1 -block_beg) / time_interval))     
-            block_beg += block_size
-            block_end = block_beg + block_size
-            shrink_size = 0
-            iteration_count += 1
-            if iteration_count == iteration_times:
-                print "Iteraion detection complete."
-                break
-        else :
-            shrink_size += shrink_rate
-            block_end -= shrink_size
-            if shrink_size >= block_size:
-                #print "can't find iteration in this block"
-                block_beg += block_size
-                block_end = block_beg + block_size
-                shrink_size = 0
-    process += 1
-
+        fuzz_ratio = fuzz.token_sort_ratio(blockString, pattern) 
+        fuzzyRatioTable.append(fuzz_ratio)
+        print("fuzz ratio:", fuzz_ratio)
+        block_beg += 2
+        block_end += 2
+        process += 2
+    #find largest fuzzy ratio n blocks (n = iteration_times)
+    ind = np.argpartition(fuzzyRatioTable, -iteration_times)[-iteration_times:]
+    for index in ind: 
+        iteration_table.append((float(index + blank_count) * time_interval, float(index + block_size + blank_count) * time_interval))     
 def eventCount(column, eventName, df):
     #get rows count that contains eventName
     return df[df[column].str.contains(eventName, na=False)][column].count()
@@ -126,51 +122,55 @@ def patternMatching(patternTable, vector, threshold):
     for index, row in patternTable.iterrows():
         #pvector is vector from patternTable without timestamp
         pvector = row.values[:-1]
-        sim = similarity(pvector, vector)
-        if sim >= threshold:
+        
+        if similar(pvector, vector):
             iteration_index = index+1
-            break
+            return 1
     return -1
 
-def similarity(a, b):
-    maxv = max(np.linalg.norm(a), np.linalg.norm(b))
-    minv = min(np.linalg.norm(a), np.linalg.norm(b))
-    if (maxv/minv)<0.5:
-        print maxv/minv
-        return 0
+def similar(a, b):
+    langa = np.linalg.norm(a)
+    langb = np.linalg.norm(b)
+    if (langa/langb)<0.4 or (langb/langa)<0.4:
+        return False
     result = 1. - spatial.distance.cosine(a, b)
-    return result
+    if result < 0.7:
+        return False
+    return True
 
  
 def traces_to_json(path):
     global iteration_begin, iteration_end, iteration_table, base_time
-    with open(path, 'w+') as f:
+    sofa_traces = None
+    with open(path, 'r') as f:
         sofa_traces = f.readlines()
+    with open(path, 'w') as f:
+        #sofa_traces = f.readlines()
         f.writelines([line for line in sofa_traces[:-1]])
         f.write("\n\n")
         f.write("iteration_detection = ")
         f.write('{"color": "rgba(241,156,162,1)", "data": [')    
         for (IT_beg, IT_end) in iteration_table:
+            print("begin:%f end:%f duration:%f"%(IT_beg+base_time,IT_end+base_time,IT_end-IT_beg))
             f.write('{"name": "iteration_begin", "x": ' + str(IT_beg + base_time) + ', "y": 1000000}, {"name": "iteration_end", "x": ' + str(IT_end + base_time) +', "y": 1000000}, ')
-        f.write(']}')
-        f.write("\n\n")
-        f.write(sofa_traces[-1].split(",")[0:-1] + "iteration_detection ]")
-
+        f.write(']}\n')
+        for s in sofa_traces[-1].split(','): 
+            if s.find(']') == -1:
+                f.write(s+',')
+        f.write("iteration_detection ]")
 def trace_timeline(path):
-    global iteration_timelines
+    global iteration_timelines, blank_count
     i = 0
     with open(path, 'w') as f:
+        for x in range(blank_count):
+            f.write(str(i) + "," + str(0))
+            i += 1
         for index in iteration_timelines:
-            f.write(str(i)+","+str(index))
+            f.write(str(i) + "," + str(index))
             i += 1
 
 def sofa_deepprof(logdir, cfg, df_cpu, df_gpu):
     global iteration_begin, iteration_end, base_time
-    filein = []
-
-    filein_gpu = logdir + "gputrace.csv"
-    filein_cpu = logdir + "cputrace.csv"
-    filein_mpstat = logdir + "mpstat_trace.csv"
 
     try:
         base_time = df_gpu.loc[0,'timestamp']
