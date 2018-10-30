5a719c26674862f59c7cd2f7cb4a009f8193bad1
cyliustack <cyliustack@gmail.com>
2018-10-30 20:15:38 +0800

    installation if /opt/conda existed

--- tools/prerequisite.sh
+++ tools/prerequisite.sh
@@ -78,13 +78,13 @@ function install_python_packages()
     echo "Install via pip"
     PIP_PACKAGES="numpy pandas scipy networkx cxxfilt fuzzywuzzy sqlalchemy sklearn"
     python3.6 -m pip install --user --upgrade pip
-    python3.6 -m pip install --user ${PIP_PACKAGES}
+    python3.6 -m pip install --user --no-cache-dir ${PIP_PACKAGES}
     [[ $? != 0 ]] && echo -e "${C_RED_BK}Failed... :(${C_NONE}" && exit 1
 
     if [[ $(which /opt/conda/bin/python3.6) ]] ; then
         echo -e "${C_GREEN}Installing conda packages...${C_NONE}"
         /opt/conda/bin/python3.6 -m pip install --user --upgrade pip
-        /opt/conda/bin/python3.6 -m pip install --user ${PIP_PACKAGES}
+        /opt/conda/bin/python3.6 -m pip install --user --no-cache-dir  ${PIP_PACKAGES}
         [[ $? != 0 ]] && echo -e "${C_RED_BK}Failed... :(${C_NONE}" && exit 1
     fi
 
