fd62da8a4eb6f776ea6589a65e38f0a4aa771e4d
Your Name <you@example.com>
2019-05-02 22:47:14 +0800

    update build_demo

--- tools/build_demo.sh
+++ tools/build_demo.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -x
 rm -r demo/sofalog 
+./bin/sofa stat "dd if=/dev/zero of=dummy.out bs=10M count=100"
 cp -ar sofalog demo
 rm demo/sofalog/sofa.pcap 
 ./bin/sofa clean --logdir demo/sofalog
 ls -alh demo/sofalog
 set +x
+
+echo "Please do 'git add demo' to include new demo updates."
 
