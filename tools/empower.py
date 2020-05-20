30fd6a3f85b5cc269c54c1a6d3683489a0dad3ba
James <cyliustack@gmail.com>
2020-05-20 00:10:28 -0400

    update empower.py

--- tools/empower.py
+++ tools/empower.py
@@ -29,30 +29,30 @@ if __name__ == '__main__':
         print(args.utility + ' is not a valid file path.')
         sys.exit(-1)
         
-    with_sudo = ''
+    sudo = ''
     if(os.path.exists('/usr/bin/sudo')):
-        with_sudo = 'sudo '
+        sudo = 'sudo '
         inform_sudo()
 
     if args.recover:     
         print('Recovering mode of user/group and capability of "' + args.utility + '"...')
-        ret = call(with_sudo + 'chgrp root ' + ' ' + args.utility, shell=True)
+        ret = call(sudo + 'chgrp root ' + ' ' + args.utility, shell=True)
         if args.remove_group_sofa:
             print('The group "sofa" is going to be removed. Are you sure (y/n)?', end=' ')
             if input() == 'y':
-                call(with_sudo + 'groupdel sofa', shell=True)
+                call(sudo + 'groupdel sofa', shell=True)
         sys.exit(0)
     else:
-        call(with_sudo + 'groupadd sofa', shell=True)
-        ret = call(with_sudo + 'chgrp sofa ' + args.utility, shell=True)
+        call(sudo + 'groupadd sofa', shell=True)
+        ret = call(sudo + 'chgrp sofa ' + args.utility, shell=True)
         if ret != 0:
             print('Failed...')
             sys.exit(1)
-        call(with_sudo + 'chmod g+rx ' + args.utility, shell=True)
+        call(sudo + 'chmod g+rx ' + args.utility, shell=True)
 
     #Set capabilities for the specified utility
     if args.utility.find('tcpdump') != -1:
-        ret = call(with_sudo + 'setcap cap_net_raw,cap_net_admin=eip ' + args.utility, shell=True)
+        ret = call(sudo + 'setcap cap_net_raw,cap_net_admin=eip ' + args.utility, shell=True)
     
     if ret != 0:
         print('Failed...')
@@ -65,8 +65,8 @@ if __name__ == '__main__':
         sys.exit(1)
     else:
         inform_sudo()
-        call(with_sudo + 'usermod -a -G sofa %s' % args.user, shell=True)
+        call(sudo + 'usermod -a -G sofa %s' % args.user, shell=True)
     print('Every thing is ready.')
 
-    #call(with_sudo + 'newgrp sofa', shell=True) 
+    #call(sudo + 'newgrp sofa', shell=True) 
     print('\n\nPlease logout and then login to make group setting effective if necessary.')
